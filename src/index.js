33b54777c9c1936f7ec47d538c2371e5d9082844
dm94 <dm94@hotmail.es>
2020-10-03 14:28:58 +0200

    remove files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render } from "react-snapshot";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 
 render(
@@ -10,8 +9,3 @@ render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
