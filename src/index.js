206e6a05b8a8bef34ac6dba2240b8600412d6dbf
dm94 <dm94@hotmail.es>
2020-10-13 19:10:07 +0200

    remove snap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { hydrate, render } from "react-dom";
+import { render } from "react-dom";
 import CrafterApp from "./CrafterApp";
 import "bootstrap/dist/css/bootstrap.min.css";
 import $ from "jquery";
@@ -7,8 +7,4 @@ import Popper from "popper.js";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<CrafterApp />, rootElement);
-} else {
-  render(<CrafterApp />, rootElement);
-}
+render(<CrafterApp />, rootElement);
