b0aefd3212856811124251e540179741a62395d4
dm94 <dm94@hotmail.es>
2020-11-17 11:56:44 +0100

    2.6.2
    - Added icons

--- src/components/Ingredient.jsx
+++ src/components/Ingredient.jsx
@@ -2,10 +2,20 @@ import React, { Component } from "react";
 import { withTranslation } from "react-i18next";
 
 class Ingredient extends Component {
+  getIcon() {
+    var res = this.props.ingredient.name.replace(" ", "_");
+    var img = new Image();
+    img.src = "https://api2.comunidadgzone.es/items/" + res + ".png";
+    if (img.complete) {
+      return <img src={img.src} alt={this.props.ingredient.name} width="2%" />;
+    }
+  }
+
   render() {
     const { t } = this.props;
     return (
       <div className="text-center">
+        {this.getIcon()}
         {this.props.ingredient.count * this.props.value}x{" "}
         {t(this.props.ingredient.name)}
       </div>
