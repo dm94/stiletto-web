b83dd0161e1c2bf47c1c83d67824dfd43a5fac70
dm94 <dm94@hotmail.es>
2021-02-06 13:39:40 +0100

    Update DiscordConnection.jsx

--- src/pages/DiscordConnection.jsx
+++ src/pages/DiscordConnection.jsx
@@ -2,9 +2,39 @@ import React, { Component } from "react";
 import PrivateProfile from "../components/PrivateProfile";
 import { withTranslation } from "react-i18next";
 import { Helmet } from "react-helmet";
+import Axios from "axios";
+import LoadingScreen from "../components/LoadingScreen";
 const queryString = require("query-string");
 
 class DiscordConnection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    const parsed = queryString.parse(this.props.location.search);
+    if (parsed.code != null) {
+      Axios.post(process.env.REACT_APP_API_URL + "/users/", {
+        params: {
+          code: parsed.code,
+        },
+      }).then((response) => {
+        if (response.status === 201) {
+          if (response.data.discordid != null) {
+            localStorage.setItem("discordid", response.data.discordid);
+          }
+          if (response.data.token != null) {
+            localStorage.setItem("token", response.data.token);
+          }
+        }
+      });
+    }
+    this.setState({ isLoaded: true });
+  }
+
   showClanInfo() {
     const { t } = this.props;
     const parsed = queryString.parse(this.props.location.search);
@@ -96,6 +126,9 @@ class DiscordConnection extends Component {
   }
 
   render() {
+    if (!this.state.isLoaded) {
+      return <LoadingScreen />;
+    }
     return <div className="h-100 container">{this.showClanInfo()}</div>;
   }
 }
