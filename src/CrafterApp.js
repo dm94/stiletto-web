5be59c7b72f94f0062e27528834bea8c155b64a1
dm94 <dm94@hotmail.es>
2021-09-25 00:39:55 +0200

    3.17.7: Added a sitemap.xml

--- src/CrafterApp.js
+++ src/CrafterApp.js
@@ -300,7 +300,7 @@ function CrafterApp() {
                   </div>
                 </div>
                 <div className="modal-footer">
-                  <p className="mr-auto">v. 3.17.6</p>
+                  <p className="mr-auto">v. 3.17.7</p>
                   <button
                     className={
                       localStorage.getItem("darkmode") !== "true"
