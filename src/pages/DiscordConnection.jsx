374c61727468d9fa08061fbbe1948f6253506feb
dm94 <dm94@hotmail.es>
2022-08-19 19:25:34 +0200

    Possible fix for the error that you can't connect with firefox

--- src/pages/DiscordConnection.jsx
+++ src/pages/DiscordConnection.jsx
@@ -40,7 +40,11 @@ class DiscordConnection extends Component {
             discordid: response.data.discordid,
             token: response.data.token,
           });
-          window.location.href = window.location.origin;
+          let http = window.location.protocol;
+          let slashes = http.concat("//");
+          let host = slashes.concat(window.location.hostname);
+          window.location.href =
+            host + (window.location.port ? ":" + window.location.port : "");
         } else if (response.status === 401) {
           this.setState({ error: "Unauthorized" });
         } else if (response.status === 503) {
