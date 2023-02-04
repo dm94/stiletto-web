73428682e2cf19f3b3203a4b0a394bd35e0e245d
Dm94 <dm94@hotmail.es>
2023-02-04 16:59:57 +0100

    Fix import

--- src/page-tracking.js
+++ src/page-tracking.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 export const usePageTracking = () => {
   const location = useLocation();
