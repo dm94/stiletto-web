0cd81e5c6814263835ad9092cfee86526fd4af84
dm94 <dm94@hotmail.es>
2024-10-01 20:47:45 +0200

    feat: Implement the suggestion

--- src/components/Wiki/DropsInfo.jsx
+++ src/components/Wiki/DropsInfo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-const DropsInfo = ({ drops }) => {
+const DropsInfo = ({ drops = [] }) => {
   const { t } = useTranslation();
 
   const showDrops = () => {
