5646adfd33f075d02a14090abccdf6729bbe80d6
Daniel Martín <dm94@hotmail.es>
2024-12-12 13:59:25 +0100

    Update src/functions/requests/recipes.js
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- src/functions/requests/recipes.js
+++ src/functions/requests/recipes.js
@@ -2,17 +2,16 @@ import { config } from "../../config/config";
 
 export const addRecipe = async (items) => {
   try {
-    const response = await fetch(`${config.REACT_APP_API_URL}/recipes`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        items: JSON.stringify(items),
-      }),
-    });
+    return await fetch(`${config.REACT_APP_API_URL}/recipes`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            items: JSON.stringify(items),
+          }),
+        });
 
-    return response;
   } catch {
     throw new Error("Error when connecting to the API");
   }
