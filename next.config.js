46b947fed01462bb083cbe071ae399083a0190b1
Dani <dm94@hotmail.es>
2025-04-18 18:33:40 +0200

    refactor: remove i18n configuration from next.config.js
    The i18n configuration was removed from the Next.js configuration file as it is no longer needed. This simplifies the config and aligns with the current project requirements.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 
-import nextI18NextConfig from './next-i18next.config.js';
-const { i18n } = nextI18NextConfig;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   distDir: './build',
-  i18n,
 }
 
 export default nextConfig
