02647c2083d1b87293699fea98bf19375b2a4e76
Daniel Martín <dm94@hotmail.es>
2025-04-30 16:39:49 +0200

    chore: comment out output export in next.config.mjs; expand generateStaticParams in page.tsx

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,7 +1,30 @@
 import ClientOnly from "./client";
 
 export function generateStaticParams() {
-  return [{ slug: [""] }];
+  return [
+    { slug: [""] },
+    { slug: ["profile"] },
+    { slug: ["crafter"] },
+    { slug: ["members"] },
+    { slug: ["clanlist"] },
+    { slug: ["maps"] },
+    { slug: ["maps", "1"] },
+    { slug: ["trades"] },
+    { slug: ["diplomacy"] },
+    { slug: ["auctions"] },
+    { slug: ["others"] },
+    { slug: ["map"] },
+    { slug: ["map", "1"] },
+    { slug: ["tech"] },
+    { slug: ["tech", "tree"] },
+    { slug: ["privacy"] },
+    { slug: ["item"] },
+    { slug: ["item", "example"] },
+    { slug: ["creature"] },
+    { slug: ["creature", "example"] },
+    { slug: ["wiki"] },
+    { slug: ["not-found"] },
+  ];
 }
 
 export default function Page() {
