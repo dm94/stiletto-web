357ad483e35a8bb32176f4806aae1750cea73911
Daniel Martín <dm94@hotmail.es>
2025-04-30 15:25:40 +0200

    chore: wrap App component in BrowserRouter for routing support

--- src/app/[[...slug]]/client.tsx
+++ src/app/[[...slug]]/client.tsx
@@ -1,9 +1,19 @@
 "use client";
 
+import "../../styles/normalize.css";
+import "../../styles/style.css";
+import "../../styles/tribal-ui.css";
+import "../../styles/desert-theme.css";
+import { BrowserRouter } from "react-router";
+import "../../i18n";
 import dynamic from "next/dynamic";
 
 const App = dynamic(() => import("../../CrafterApp"), { ssr: false });
 
 export default function ClientOnly() {
-  return <App />;
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 }
