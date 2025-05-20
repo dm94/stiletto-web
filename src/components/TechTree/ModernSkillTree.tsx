b6b2e582cd5c84e9c580c54a0b803b7f734b7eb7
Daniel Martín <dm94@hotmail.es>
2025-05-21 00:30:13 +0200

    refactor: Optimize flattenTree function for better performance

--- src/components/TechTree/ModernSkillTree.tsx
+++ src/components/TechTree/ModernSkillTree.tsx
@@ -147,7 +147,9 @@ const ModernSkillTree: React.FC<ModernSkillTreeProps> = ({
     // Flatten the tree for easier processing
     const flattenTree = (nodes: NodeData[]): NodeData[] => {
       return nodes.reduce((acc, node) => {
-        return [...acc, node, ...flattenTree(node.children)];
+        acc.push(node);
+        acc.push(...flattenTree(node.children));
+        return acc;
       }, [] as NodeData[]);
     };
 
