378829b77e2567b86144c32cd37039139acc48d9
Razvan <40596596+rxrdev@users.noreply.github.com>
2026-02-10 16:04:36 +0200

    Add z-index to DeleteMapModal for improved stacking (#365)

--- src/components/ClanMaps/DeleteMapModal.tsx
+++ src/components/ClanMaps/DeleteMapModal.tsx
@@ -21,7 +21,7 @@ const DeleteMapModal: React.FC<DeleteMapModalProps> = ({
         <div className="fixed inset-0 transition-opacity" aria-hidden="true">
           <div className="absolute inset-0 bg-gray-900 opacity-75" />
         </div>
-        <div className="inline-block align-bottom bg-gray-800 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+        <div className="inline-block z-10 relative align-bottom bg-gray-800 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
           <div className="bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div className="sm:flex sm:items-start">
               <div className="mt-3 text-center sm:mt-0 sm:text-left">
